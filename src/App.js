938a9d7299455ee09b76f06b428770193396da7c
Joel Ajay <64684370+joel2607@users.noreply.github.com>
2023-02-28 17:26:14 +0530

    Connected to localserver running on port 8000.
    - Made the todos load from a server running on port 8000 using axios and express.
    - Added done/undone functionality to the design.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,66 @@
 import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import Axios from 'axios';
 
-function TodoContainer({name, onDone}){
+const server = Axios.create({
+  baseURL: "http://localhost:9000/"
+});
+
+function TodoContainer({task, onDone}){
   return (
     <div>
-      <label>{name}</label>
-      <input type = "checkbox" onClick={onDone()}></input>
+      <label 
+        style = {{
+          textDecoration: (task.done)?'line-through':'none'
+        }}
+        onClick = {onDone}>
+          {task.name}
+      </label>
     </div>
   );
 }
 
-function TodoSubmitArea(){
+function TodoSubmitArea({onAdd}){
+
   return (
     <div>
-      <input type = "text">
-        
-      </input>
-      <button>Submit</button>
+      <input type = "text"></input>
+      <button>Add</button>
     </div>
   );
 }
 
 function App() {
-  function onDone(i){
-    console.log("done" + i);
+  const [tasks, setTasks] = useState([]);
+  
+  useEffect(() => {
+    server.get('/')
+    .then((response) => {
+      setTasks(response.data)
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }, []);
+  
+  function handleDone(id){
+    let new_tasks = tasks.slice() 
+    let task_handled = new_tasks[new_tasks.findIndex( task => task.id === id)];
+    task_handled.done = !task_handled.done;
+    setTasks(new_tasks);
   }
+
+  const taskElements = tasks.map((task) => {
+    return (
+      <TodoContainer key = {task.id} task = {task} onDone = {() => handleDone(task.id)}></TodoContainer>
+    )
+  });
   return (
-    <>
-      <TodoContainer name = "Task 1" onDone = {() => onDone(1)}></TodoContainer>
+    <div className='App'>
       <TodoSubmitArea></TodoSubmitArea>
-    </>
+      {taskElements}
+    </div>
   );
 }
 
