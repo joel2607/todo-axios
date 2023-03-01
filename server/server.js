b3e1de51c813c1e3bac5e7f31918e5b1f44551c7
Joel Ajay <64684370+joel2607@users.noreply.github.com>
2023-03-01 21:43:11 +0530

    Removed dummy values in server

--- server/server.js
+++ server/server.js
@@ -7,18 +7,7 @@ const app = express()
 app.use(cors());
 app.use(express.json());
 
-const tasks = [
-    {
-        id: 1,
-        name: "Server Task 1",
-        done: false
-    },
-    {
-        id: 2,
-        name: "Server Task 2",
-        done: true
-    }
-];
+const tasks = [];
 
 app.get("/", (req, res) => {
     res.send(tasks);
