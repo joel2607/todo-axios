a7f9d31b5df21527bd7b52657e910b97eb2f3fd3
Joel Ajay <ajaijoel@gmail.com>
2023-03-01 21:32:36 +0530

    -Added ability to add new tasks - Added serverside ability to update tasks provided id given (front end implementation of this feature not done yet)

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express"); 
 const cors = require('cors');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express() 
 
@@ -23,12 +24,23 @@ app.get("/", (req, res) => {
     res.send(tasks);
 });
 
-app.put("/", (req, res) => {
-    tasks.push(req.body);
-    res.send(tasks);
+app.post("/", (req, res) => {
+    const newTask = {
+        id: uuidv4(),
+        name: req.body.text,
+        done: false
+    }
+    tasks.push(newTask);
+    res.status(201).json(newTask);
 })
 
-// app.post()
+app.put("/", (req,res) => {
+    const newTask = req.body;
+    let existingTaskIndex = tasks.findIndex((taskItem) => taskItem.id == newTask.id);
+    if (existingTaskIndex == -1) tasks.push(newTask); // Array.Prototype.findIndex() returns -1 if item is not found
+    else tasks[existingTaskIndex] = newTask;
+    res.status(201).json(tasks);
+});
 
 app.listen(9000, ()=>{
     console.log('server running at 9000')
